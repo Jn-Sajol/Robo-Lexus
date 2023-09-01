9a57ce42b7355cc2cff97f00a0c6804d924cb5e7
Jn-Sajol <julkar10121@gmail.com>
2023-09-01 22:54:27 +0600

    adding data fatchinf part

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Component } from "react";
-
-class App extends Component() {
-  render(){
+import { useState } from "react";
+const App = () => {
+  const [users,setUser] = useState();
+  // const fetchData = () =>{
+    // fetch("https://jsonplaceholder.typicode.com/users")
+    // .then((res) =>{
+    //   return res.json();
+    // }).then((data) =>{
+    //   setUser(data);
+    //   // console.log(data)
+    // })
+    
+  
     return (
       <div className="App">
         <header className="App-header">
-          <h1>hello wold</h1>
-          <h3>there is the rmain </h3>
-          hello
+          <h1>hwllo</h1>
+          {/* {
+            // users?.map(user =>{
+            <div>
+              <h1>{user.name}</h1>
+            </div>
+            })
+          } */}
         </header>
       </div>
     );
-  }
 }
 
 export default App;
+

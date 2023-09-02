244990042621846aa136a53c0c5b81dd8d6e73cd
Jn-Sajol <julkar10121@gmail.com>
2023-09-02 09:56:42 +0600

    adding filter sehent

--- src/App.js
+++ src/App.js
@@ -1,33 +1,52 @@
+
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
 const App = () => {
-  const [users,setUser] = useState();
-  // const fetchData = () =>{
-    // fetch("https://jsonplaceholder.typicode.com/users")
-    // .then((res) =>{
-    //   return res.json();
-    // }).then((data) =>{
-    //   setUser(data);
-    //   // console.log(data)
-    // })
-    
-  
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>hwllo</h1>
-          {/* {
-            // users?.map(user =>{
-            <div>
-              <h1>{user.name}</h1>
-            </div>
-            })
-          } */}
-        </header>
-      </div>
+  const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filteredUsers, setFilteredUsers] = useState([]); // New state for filtered users
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+        setFilteredUsers(data); // Initialize filteredUsers with all users
+      });
+  };
+
+  useEffect(() => {
+    // Filter users when the search query changes
+    const filtered = users.filter((user) =>
+      user.name.toLowerCase().includes(search.toLowerCase())
     );
-}
+    setFilteredUsers(filtered);
+  }, [search, users]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello</h1>
+        <input
+          type="search"
+          value={search}
+          placeholder="Search the robots"
+          onChange={(event) => setSearch(event.target.value)}
+        />
+        {filteredUsers.map((user) => (
+          <div key={user.id}>
+            <h1>{user.name}</h1>
+          </div>
+        ))}
+      </header>
+    </div>
+  );
+};
 
 export default App;
-

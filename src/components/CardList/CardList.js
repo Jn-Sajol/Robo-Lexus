671bcd07d59452be68286802877e2ae1b871f5a6
Jn-Sajol <julkar10121@gmail.com>
2023-10-06 12:32:29 +0600

    adding contxt

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -4,10 +4,8 @@ const CardList = ({ user }) => {
   return (
     <div className="card-list">
       <div className="card-container">
-        <div>
         <h1>{user.name}</h1>
         <h1>{user.email}</h1>
-        </div>
       </div>
     </div>
   );

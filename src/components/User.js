d45e7997db5f46f87c6f12e0541b39bf6b2a1d55
Jn-Sajol <julkar10121@gmail.com>
2023-10-06 12:40:49 +0600

    adding contxt

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { userContext } from '../context/userContext'
 
 const User = () => {
-    
+    const users = useContext(userContext);
+    console.log(users);
   return (
     <div>User</div>
   )
